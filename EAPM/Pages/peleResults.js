3bab3ba2ca4c15d75d707289d2b1efc73b08e15f
Christian González <chrisgonzaco@gmail.com>
2024-02-07 14:02:15 +0100

    implementing multiple proteins and ligands

--- EAPM/Pages/peleResults.js
+++ EAPM/Pages/peleResults.js
@@ -3,16 +3,26 @@ const peleSimulationFolderBtn = document.querySelector('#peleSimulationFolderBtn
 const peleOutputFolder = document.querySelector('#peleOutputFolder')
 const peleForm = document.querySelector('#peleForm')
 const plotForm = document.querySelector('#plotForm')
-const plotButton = document.querySelector('#plotButton')
 const proteinSelector = document.querySelector('#proteinSelector')
 const ligandSelector = document.querySelector('#ligandSelector')
 const distanceSelector = document.querySelector('#distanceSelector')
 const colorSelector = document.querySelector('#colorSelector')
 
 let fetchData
+let dataColumn
 
 const addOption = (selector, array) => {
     array.forEach(i => {
+        option = document.createElement('option')
+        option.id = i
+        option.value = i
+        option.innerHTML = i
+        selector.append(option)
+    });
+}
+
+const addDictOption = (selector, array) => {
+    Object.keys(array).forEach(i => {
         option = document.createElement('option')
         option.id = i
         option.value = i
@@ -26,11 +36,11 @@ peleSimulationFolderBtn.addEventListener("click", (e) => {
 
     const href = window.location.href
 
-    let result = {}
-
     const data = {
         peleSimulationFolder: peleSimulationFolder.value,
-        peleOutputFolder: peleOutputFolder.value
+        peleOutputFolder: peleOutputFolder.value,
+        desiredProtein: null,
+        desiredLigand: null
     }
 
     fetch(href + '/testEndpoint', {
@@ -46,90 +56,154 @@ peleSimulationFolderBtn.addEventListener("click", (e) => {
                 fetchData = parsedData
                 console.log(parsedData)
 
-                const proteins = parsedData.proteins
-                const ligands = parsedData.ligands
-                const distances = parsedData.distances
+                const proteins = parsedData.distances
+                const proteinValue = Object.keys(proteins)[0]
+                const ligands = parsedData.distances[proteinValue]
+                const distances = parsedData.distances[proteinValue][Object.keys(ligands)[0]]
+                
+                addDictOption(proteinSelector, proteins)
 
-                addOption(proteinSelector, proteins)
-                addOption(ligandSelector, ligands)
+                addDictOption(ligandSelector, ligands)
+
                 addOption(distanceSelector, distances)
+
+                console.log(distances)
 
                 peleForm.style.display = 'none'
 
                 plotForm.style.display = 'block'
+
+                const bindingEnergyIndex = fetchData.dict.columns.indexOf("Binding Energy")
+
+                dataColumn = fetchData.dict.data.map(row => row[bindingEnergyIndex])
+
+                showPlot()
+            }else{
+                console.log('Error: ' + parsedData.msg)
             }
         })
         .catch(error => console.error('Error en la solicitud:', error));
 })
 
-plotButton.addEventListener("click", (e) => {
-    e.preventDefault()
-
+const showPlot = () => {
     let x = fetchData.dict.columns[6];
     let y = fetchData.dict.columns[7];
 
     let distanceValue = distanceSelector.value
+    const distanceColumnIndex = fetchData.dict.columns.indexOf(distanceValue);
+    distanceColumn = fetchData.dict.data.map(row => row[distanceColumnIndex]);
 
-    const totalEnergy = fetchData.dict.columns.indexOf("Binding Energy");
     let colorSelectorIndex = fetchData.dict.columns.indexOf(colorSelector.value);
-    const distanceColumn = fetchData.dict.columns.indexOf(distanceValue);
-
-    const columnData = fetchData.dict.data.map(row => row[totalEnergy]);
-    const columnDistance = fetchData.dict.data.map(row => row[distanceColumn]);
     let colorSelectorColumn = fetchData.dict.data.map(row => row[colorSelectorIndex]);
-
 
     if(colorSelector.value == 'None'){
         let trace = {
-            x: columnData,
-            y: columnDistance,
+            x: distanceColumn,
+            y: dataColumn,
             mode: 'markers',
             type: 'scatter',
             marker: {
-                size: 10,
-                color: 'rgba(50, 171, 96, 0.7)',
+                size: 6,
+                color: 'rgba(45, 85, 255, 0.7)',    
             }
         };
     
         let layout = {
             title: 'Scatter plot',
+            width: 500,
+            height: 500,
             xaxis: {
                 title: distanceValue
             },
             yaxis: {
                 title: 'Binding energy'
-            }
+            },
+            marker: { size: 3 }
         };
     
         Plotly.newPlot('testPlot', [trace], layout);
     }else{
         let trace = {
-            x: columnData,
-            y: columnDistance,
+            x: distanceColumn,
+            y: dataColumn,
             mode: 'markers',
             type: 'scatter',
             marker: {
-                size: 10,
+                size: 4,
                 color: colorSelectorColumn,
                 colorscale: 'Viridis',
                 colorbar: {
                     title: colorSelector.value
                 }
-            }
+            },
         };
         
         let layout = {
             title: 'Scatter plot',
+            width: 600,
+            height: 500,
             xaxis: {
                 title: distanceValue
             },
             yaxis: {
                 title: 'Binding energy'
-            }
+            },
+            
         };
-        
         Plotly.newPlot('testPlot', [trace], layout);
     }
+}
+
+plotForm.addEventListener("change", (e) => {
+
+    if(e.target.id === "proteinSelector"){
+
+        const data = {
+            peleSimulationFolder: peleSimulationFolder.value,
+            peleOutputFolder: peleOutputFolder.value,
+            desiredProtein: proteinSelector.value,
+            desiredLigand: ligandSelector.value
+        }
+
+        fetch(href + '/testEndpoint', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(parsedData => {
+                if(parsedData.ok){
+                    fetchData = parsedData
+                    console.log(parsedData)
+    
+                    // const proteins = parsedData.distances
+                    // const proteinValue = Object.keys(proteins)[0]
+                    // const ligands = parsedData.distances[proteinValue]
+                    // const distances = parsedData.distances[proteinValue][Object.keys(ligands)[0]]
+                    
+                    ligandSelector.innerHTML = ''
+                    distanceSelector.innerHTML = ''
+
+                    const proteinValue = e.target.value
+                    const ligands = parsedData.distances[proteinValue]
+                    const distances = parsedData.distances[proteinValue][ligandSelector.value]
+
+                    addDictOption(ligandSelector, ligands)
+
+                    addOption(distanceSelector, distances)
+                    
+                    showPlot()
+                }else{
+                    console.log('Error: ' + parsedData.msg)
+                }
+            })
+            .catch(error => console.error('Error en la solicitud:', error));
+    }
+
+
+    showPlot()
 })
 
 
