c925707ca9a0b16c9c709d8949065a66209ede71
Christian González <chrisgonzaco@gmail.com>
2024-02-13 12:31:34 +0100

    added plotlyClick endpoint

--- EAPM/Include/Pages/peleResults.py
+++ EAPM/Include/Pages/peleResults.py
@@ -9,11 +9,90 @@ peleResultsPage = PluginPage(
     html="peleResults.html"
 )
 
+def returnDataframe(self, protein, ligand, vertical_line=None, color_column=None, size=1.0, labels_size=10.0, plot_label=None,
+                                        xlim=None, ylim=None, metrics=None, labels=None, title=None, title_size=14.0, return_axis=False, dpi=300, show_legend=False,
+                                        axis=None, xlabel=None, ylabel=None, vertical_line_color='k', vertical_line_width=0.5, marker_size=0.8, clim=None, show=False,
+                                        clabel=None, legend_font_size=6, no_xticks=False, no_yticks=False, no_cbar=False, no_xlabel=False, no_ylabel=False,
+                                        relative_color_values=False, dataframe=None, **kwargs):
+        """
+        Creates a scatter plot for the selected protein and ligand using the x and y
+        columns. Data series can be filtered by specific metrics.
 
+        Parameters
+        ==========
+        protein : str
+            The target protein.
+        ligand : str
+            The target ligand.
+        x : str
+            The column name of the data to plot in the x-axis.
+        y : str
+            The column name of the data to plot in the y-axis.
+        vertical_line : float
+            Position to plot a vertical line.
+        color_column : str
+            The column name to use for coloring the plot. Also a color cna be given
+            to use uniformly for the points.
+        xlim : tuple
+            The limits for the x-range.
+        ylim : tuple
+            The limits for the y-range.
+        clim : tuple
+            The limits for the color range.
+        metrics : dict
+            A set of metrics for filtering the data points.
+        labels : dict
+            Analog to metrics, use the label column values to filter the data.
+        title : str
+            The plot's title.
+        return_axis : bool
+            Whether to return the axis of this plot.
+        axis : matplotlib.pyplot.axis
+            The axis to use for plotting the data.
+        """
+        try:
+            if not isinstance(dataframe, type(None)):
+                protein_series = dataframe[dataframe.index.get_level_values('Protein') == protein]
+            else:
+                protein_series = self.data[self.data.index.get_level_values('Protein') == protein]
 
-def getDistanceByProtein(pele):
-    pass
+            if protein_series.empty:
+                raise ValueError('Protein name %s not found in data!' % protein)
+            ligand_series = protein_series[protein_series.index.get_level_values('Ligand') == ligand]
 
+            if ligand_series.empty:
+                raise ValueError("Ligand name %s not found in protein's %s data!" % (ligand, protein))
+
+            # Add distance data to ligand_series
+            if len(ligand_series) != 0:
+                if protein in self.distances:
+                    if ligand in self.distances[protein]:
+                        if not isinstance(self.distances[protein][ligand], type(None)):
+                            for distance in self.distances[protein][ligand]:
+                                #if distance.startswith('distance_'):
+
+                                if not isinstance(dataframe, type(None)):
+                                    
+                                    indexes = dataframe.reset_index().set_index(['Protein', 'Ligand', 'Epoch', 'Trajectory', 'Accepted Pele Steps', 'Step']).index
+                                    ligand_series[distance] = self.distances[protein][ligand][self.distances[protein][ligand].index.isin(indexes)][distance].tolist()
+                                else:
+                                    ligand_series[distance] = self.distances[protein][ligand][distance].tolist()
+
+            # Filter points by metric
+            if not isinstance(metrics, type(None)):
+                for metric in metrics:
+                    mask = ligand_series[metric] <= metrics[metric]
+                    ligand_series = ligand_series[mask]
+
+            if not isinstance(labels, type(None)):
+                for label in labels:
+                    if labels[label] != None:
+                        mask = ligand_series[label] == labels[label]
+                        ligand_series = ligand_series[mask]
+            return ligand_series
+        except Exception as e:
+            print(e)
+            return {"error": e}
 
 def getPeleResults():
     import pele_analysis
@@ -42,34 +121,34 @@ def getPeleResults():
                 pele = pele_analysis.peleAnalysis(peleFolder, verbose=True, separator='-', trajectories=False, data_folder_name=peleOutputFolder, read_equilibration=True)
                 
                 # Classify distances into common metrics --> the catalytic labels
-                # catalytic_names = ['SER-L', 'SER-HIS', 'HIS-ASP']
+                catalytic_names = ['SER-L', 'SER-HIS', 'HIS-ASP']
 
-                # catalytic_labels = {}
-                # for cn in catalytic_names:
-                #     catalytic_labels[cn] = {}
-                #     for protein in pele.proteins:
-                #         if protein not in catalytic_labels[cn]:
-                #             catalytic_labels[cn][protein] = {}
-                #         for ligand in pele.ligands:
-                #             if ligand not in catalytic_labels[cn][protein]:
-                #                 catalytic_labels[cn][protein][ligand] = []
+                catalytic_labels = {}
+                for cn in catalytic_names:
+                    catalytic_labels[cn] = {}
+                    for protein in pele.proteins:
+                        if protein not in catalytic_labels[cn]:
+                            catalytic_labels[cn][protein] = {}
+                        for ligand in pele.ligands:
+                            if ligand not in catalytic_labels[cn][protein]:
+                                catalytic_labels[cn][protein][ligand] = []
 
-                # for protein in pele.proteins:
-                #     for ligand in pele.ligands:
-                #         distances = pele.getDistances(protein, ligand)
-                #         if distances == None:
-                #             continue
-                #         for d in distances:
-                #             at1 = d.replace('distance_','').split('_')[0]
-                #             at2 = d.replace('distance_','').split('_')[1]
-                #             if at1.endswith('OG') and at2.startswith('L'):
-                #                 catalytic_labels['SER-L'][protein][ligand].append(d)
+                for protein in pele.proteins:
+                    for ligand in pele.ligands:
+                        distances = pele.getDistances(protein, ligand)
+                        if distances == None:
+                            continue
+                        for d in distances:
+                            at1 = d.replace('distance_','').split('_')[0]
+                            at2 = d.replace('distance_','').split('_')[1]
+                            if at1.endswith('OG') and at2.startswith('L'):
+                                catalytic_labels['SER-L'][protein][ligand].append(d)
 
-                #             elif at1.endswith('OG') and at2.endswith('NE2'):
-                #                 catalytic_labels['SER-HIS'][protein][ligand].append(d)
+                            elif at1.endswith('OG') and at2.endswith('NE2'):
+                                catalytic_labels['SER-HIS'][protein][ligand].append(d)
 
-                #             elif at1.endswith('ND1') and (at2.endswith('OD1') or at2.endswith('OD2')):
-                #                 catalytic_labels['HIS-ASP'][protein][ligand].append(d)
+                            elif at1.endswith('ND1') and (at2.endswith('OD1') or at2.endswith('OD2')):
+                                catalytic_labels['HIS-ASP'][protein][ligand].append(d)
 
 
                 distances = {}
@@ -92,11 +171,17 @@ def getPeleResults():
                     if firstProteinCouple:
                         break
 
-                # pele.combineDistancesIntoMetrics(catalytic_labels, overwrite=True)
+                pele.combineDistancesIntoMetrics(catalytic_labels, overwrite=True)
 
-                dict = pele.returnDataframe(firstProteinCouple, firstLigandCouple)
+                dict = returnDataframe(pele, firstProteinCouple, firstLigandCouple)
 
-                return jsonify({"ok": True, "distances": distances, "proteins": pele.proteins, "ligands": pele.ligands, "dict": dict.to_dict(orient='split')})
+                df_flat_index = dict.reset_index()
+
+                df_flat_index.fillna(0, inplace=True)
+
+                data_dict = df_flat_index.to_dict(orient='split')
+
+                return jsonify({"ok": True, "distances": distances, "proteins": pele.proteins, "ligands": pele.ligands, "dict": data_dict})
             else:
                 peleSimFolder = data['peleSimulationFolder']
 
@@ -128,21 +213,52 @@ def getPeleResults():
                 desiredProtein = data['desiredProtein']
                 desiredLigand = data['desiredLigand']
 
-                dict = pele.returnDataframe(pele.proteins[pele.proteins.index(desiredProtein)], pele.ligands[pele.ligands.index(desiredLigand)])
+                dict = returnDataframe(pele, pele.proteins[pele.proteins.index(desiredProtein)], pele.ligands[pele.ligands.index(desiredLigand)])
 
-                return jsonify({"ok": True, "dict": dict.to_dict(orient='split'), "distances": distances})
+                df_flat_index = dict.reset_index()
+
+                data_dict = df_flat_index.to_dict(orient='split')
+
+                return jsonify({"ok": True, "dict": data_dict, "distances": distances})
 
         return jsonify({"ok": False, "msg": "No pele simulations folder provided"})
     except Exception as e:
             print(f'Error: {e}')
             return jsonify({"ok": False, "msg": str(e)})
 
+def getPlotlyCLick():
+    import pele_analysis
+    from flask import jsonify
+
+    data = request.json
+
+    if "distance" and "bindingEnergy" in data:
+
+        distance = data['distance']
+        bindingEnergy = data['bindingEnergy']
+
+        print(f'{distance} - {bindingEnergy}')
+        return jsonify({"ok": True, "msg": f'{distance} - {bindingEnergy}'})
+
+    try:
+        pass
+    except Exception as e:
+        print(f'Error: {e}')
+        return jsonify({"ok": False, "msg": str(e)})
+
 # Add the endpoint to the PluginPage
-testEndpoint = PluginEndpoint(
-    url="/testEndpoint",
+peleResults = PluginEndpoint(
+    url="/peleResults",
     methods=["POST"],
     function=getPeleResults,
 )
 
+plotlyCLick = PluginEndpoint(
+    url="/plotlyClick",
+    methods=["POST"],
+    function=getPlotlyCLick,
+)
+
 # Add the endpoint to the page
-peleResultsPage.addEndpoint(testEndpoint)
+peleResultsPage.addEndpoint(peleResults)
+peleResultsPage.addEndpoint(plotlyCLick)
--- EAPM/Pages/peleResults.js
+++ EAPM/Pages/peleResults.js
@@ -7,6 +7,10 @@ const proteinSelector = document.querySelector('#proteinSelector')
 const ligandSelector = document.querySelector('#ligandSelector')
 const distanceSelector = document.querySelector('#distanceSelector')
 const colorSelector = document.querySelector('#colorSelector')
+const loader = document.querySelector('#loader')
+
+
+
 
 let fetchData
 let dataColumn
@@ -16,6 +20,8 @@ const horusData = window.parent.extensionData;
 if(horusData?.peleFolder !== undefined){
     peleSimulationFolder.innerHTML = horusData.peleFolder
 }
+
+//   parent.molstar.loadPDB(pdbData, label)
 
 const addOption = (selector, array) => {
     array.forEach(i => {
@@ -49,7 +55,8 @@ peleSimulationFolderBtn.addEventListener("click", (e) => {
         desiredLigand: null
     }
 
-    fetch(href + '/testEndpoint', {
+    loader.style.display = 'block'
+    fetch(href + '/peleResults', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -83,18 +90,154 @@ peleSimulationFolderBtn.addEventListener("click", (e) => {
 
                 dataColumn = fetchData.dict.data.map(row => row[bindingEnergyIndex])
 
+                loader.style.display = 'none'
+
                 showPlot()
+
+                pelePlot.on("plotly_click", (data) => {
+                    const d = data.points[0];
+
+                    const distance = d.x 
+                    const bindingEnergy = d.y
+
+                    const href = window.location.href
+
+                    const plotlyData = {
+                        distance: distance,
+                        bindingEnergy: bindingEnergy
+                    }
+
+                    fetch(href + '/plotlyClick', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(plotlyData)
+                    })
+                        .then(response => response.json())
+                        .then(parsedData => {
+                            if(parsedData.ok){
+                                console.log(parsedData.msg)
+                            }
+                        })
+
+                    console.log(d)
+                    // addRowToTable(d);
+                });
             }else{
                 console.log('Error: ' + parsedData.msg)
             }
         })
-        .catch(error => console.error('Error en la solicitud:', error));
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+            console.log('JSON del error:', error.response);
+        });
+    
 })
 
-const showPlot = () => {
-    let x = fetchData.dict.columns[6];
-    let y = fetchData.dict.columns[7];
+const showGrid = () => {
+    const proteinColumnIndex = fetchData.dict.columns.indexOf('Protein');
+    const proteinColumn = fetchData.dict.data.map(row => row[proteinColumnIndex]);
 
+    const ligandColumnIndex = fetchData.dict.columns.indexOf('Ligand');
+    const ligandColumn = fetchData.dict.data.map(row => row[ligandColumnIndex]);
+    
+    const epochColumnIndex = fetchData.dict.columns.indexOf('Epoch');
+    const epochColumn = fetchData.dict.data.map(row => row[epochColumnIndex]);
+
+    const trajectoryColumnIndex = fetchData.dict.columns.indexOf('Trajectory');
+    const trajectoryColumn = fetchData.dict.data.map(row => row[trajectoryColumnIndex]);
+
+    const acceptedPeleStepsColumnIndex = fetchData.dict.columns.indexOf('Accepted Pele Steps');
+    const acceptedPeleStepsColumn = fetchData.dict.data.map(row => row[acceptedPeleStepsColumnIndex]);
+
+    const stepColumnIndex = fetchData.dict.columns.indexOf('Step');
+    const stepColumn = fetchData.dict.data.map(row => row[stepColumnIndex]);
+
+    const totalEnergyColumnIndex = fetchData.dict.columns.indexOf('Total Energy');
+    const totalEnergyColumn = fetchData.dict.data.map(row => row[totalEnergyColumnIndex]);
+
+    const bindingEnergyColumnIndex = fetchData.dict.columns.indexOf('Binding Energy');
+    const bindingEnergyColumn = fetchData.dict.data.map(row => row[bindingEnergyColumnIndex]);
+
+    const ligandSASAColumnIndex = fetchData.dict.columns.indexOf('Ligand SASA');
+    const ligandSASAColumn = fetchData.dict.data.map(row => row[ligandSASAColumnIndex]);
+
+    const metricSERLColumnIndex = fetchData.dict.columns.indexOf('metric_SER-L');
+    const metricSERLColumn = fetchData.dict.data.map(row => row[metricSERLColumnIndex]);
+
+    const metricSERHISColumnIndex = fetchData.dict.columns.indexOf('metric_SER-HIS');
+    const metricSERHISColumn = fetchData.dict.data.map(row => row[metricSERHISColumnIndex]);
+
+    const metricHISASPColumnIndex = fetchData.dict.columns.indexOf('metric_HIS-ASP');
+    const metricHISASPColumn = fetchData.dict.data.map(row => row[metricHISASPColumnIndex]);
+
+    const columnNames = [
+        "Protein",
+        "Ligand",
+        "Epoch",
+        "Trajectory",
+        "Accepted Pele Steps",
+        "Step",
+        "Total Energy",
+        "Binding Energy",
+        "Ligand SASA",
+        "metric_SER-L",
+        "metric_SER-HIS",
+        "metric_HIS-ASP"
+    ];
+    
+    const distances = fetchData.distances[proteinSelector.value][ligandSelector.value]
+
+    console.log(distances)
+    
+    // Combina columnNames con las distancias dinámicas
+    const allColumnNames = [...columnNames, ...distances];
+    
+    console.log(allColumnNames)
+
+    // Mapea los nombres de las columnas a las definiciones de columnas de ag-Grid
+    const columnDefs = allColumnNames.map(column => {
+        return { field: column };
+    });
+    
+    const rowData = proteinColumn.map((protein, index) => {
+        return {
+            "Protein": protein,
+            "Ligand": ligandColumn[index],
+            "Epoch": epochColumn[index],
+            "Trajectory": trajectoryColumn[index],
+            "Accepted Pele Steps": acceptedPeleStepsColumn[index],
+            "Step": stepColumn[index],
+            "Total Energy": totalEnergyColumn[index],
+            "Binding Energy": bindingEnergyColumn[index],
+            "Ligand SASA": ligandSASAColumn[index],
+            "metric_SER-L": metricSERLColumn[index],
+            "metric_SER-HIS": metricSERHISColumn[index],
+            "metric_HIS-ASP": metricHISASPColumn[index]
+        };
+    });
+
+    distances.forEach((distance, distanceIndex) => {
+        const distanceColumnIndex = 12 + distanceIndex;
+        const distanceColumn = fetchData.dict.data.map(row => row[distanceColumnIndex]);
+    
+        rowData.forEach((row, rowIndex) => {
+            row[distance] = distanceColumn[rowIndex];
+        });
+    });
+
+    const gridOptions = {
+        rowData: rowData,
+        columnDefs: columnDefs,
+        suppressSizeToFit: true
+    };
+    
+    const gridElement = document.querySelector('#myGrid');
+    const gridApi = agGrid.createGrid(gridElement, gridOptions);
+}
+
+const showPlot = () => {
     let distanceValue = distanceSelector.value
     const distanceColumnIndex = fetchData.dict.columns.indexOf(distanceValue);
     distanceColumn = fetchData.dict.data.map(row => row[distanceColumnIndex]);
@@ -102,20 +245,23 @@ const showPlot = () => {
     let colorSelectorIndex = fetchData.dict.columns.indexOf(colorSelector.value);
     let colorSelectorColumn = fetchData.dict.data.map(row => row[colorSelectorIndex]);
 
+    showGrid()
+
     if(colorSelector.value == 'None'){
-        let trace = {
-            x: distanceColumn,
-            y: dataColumn,
-            mode: 'markers',
+        var pelePlot = document.getElementById('pelePlot'),
+        data = [{
+            x: distanceColumn, 
+            y: dataColumn, 
             type: 'scatter',
+            mode: 'markers', 
             marker: {
                 size: 6,
                 color: 'rgba(45, 85, 255, 0.7)',    
             }
-        };
-    
-        let layout = {
+                }],
+        layout = {
             title: 'Scatter plot',
+            hovermode: 'closest',
             width: 500,
             height: 500,
             xaxis: {
@@ -127,7 +273,7 @@ const showPlot = () => {
             marker: { size: 3 }
         };
     
-        Plotly.newPlot('pelePlot', [trace], layout);
+        Plotly.newPlot('pelePlot', data, layout);
     }else{
         let trace = {
             x: distanceColumn,
@@ -173,14 +319,21 @@ plotForm.addEventListener("change", (e) => {
             desiredLigand: Object.keys(fetchData.distances[proteinSelector.value])[0]
         }
 
-        fetch(href + '/testEndpoint', {
+        proteinSelector.disabled = true
+        ligandSelector.disabled = true
+        distanceSelector.disabled = true
+        colorSelector.disabled = true
+        loader.style.display = 'block'
+        fetch(href + '/peleResults', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
-            .then(response => response.json())
+            .then(response => {
+                response.json()
+            })
             .then(parsedData => {
                 if(parsedData.ok){
                     fetchData = parsedData
@@ -198,7 +351,17 @@ plotForm.addEventListener("change", (e) => {
 
                     addOption(distanceSelector, distances)
 
+                    proteinSelector.disabled = false
+                    ligandSelector.disabled = false
+                    distanceSelector.disabled = false
+                    colorSelector.disabled = false
+                    loader.style.display = 'none'
+
                     showPlot()
+
+                    pelePlot.on('plotly_click', function(){
+                        alert('You clicked this Plotly chart!');
+                    });
                 }else{
                     console.log('Error: ' + parsedData.msg)
                 }
@@ -214,6 +377,11 @@ plotForm.addEventListener("change", (e) => {
             desiredLigand: ligandSelector.value
         }
 
+        proteinSelector.disabled = true
+        ligandSelector.disabled = true
+        distanceSelector.disabled = true
+        colorSelector.disabled = true
+        loader.style.display = 'block'
         fetch(href + '/testEndpoint', {
             method: 'POST',
             headers: {
@@ -234,6 +402,12 @@ plotForm.addEventListener("change", (e) => {
 
                     addOption(distanceSelector, distances)
 
+                    proteinSelector.disabled = false
+                    ligandSelector.disabled = false
+                    distanceSelector.disabled = false
+                    colorSelector.disabled = false
+                    loader.style.display = 'none'
+
                     showPlot()
                 }else{
                     console.log('Error: ' + parsedData.msg)
@@ -244,7 +418,8 @@ plotForm.addEventListener("change", (e) => {
 
 
     showPlot()
+
+    pelePlot.on('plotly_click', function(){
+        alert('You clicked this Plotly chart!');
+    });
 })
-
-
-
